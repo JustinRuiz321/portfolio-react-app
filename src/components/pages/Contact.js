c892b8c46f9f360cab883458c8c01c6888e3d4bf
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-21 16:25:06 -0500

    Contact me page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        Email: <a href='mailto: ruiz.justin21@gmail.com'>ruiz.justin21@gmail.com</a>
+        Phone Number: <a href='tel: 5517952880'>551-795-2880</a>
+      </p>
+    </div>
+  );
+}
