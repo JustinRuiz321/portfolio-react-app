9e365f443cd8a75a73fb50030b62fa9b6b2b1275
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-22 14:30:50 -0500

    Need to figure out how to get images to fit in my card for projects page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Myself from './Myself.png';
 
 export default function Home() {
   return (
-    <div>
+    <div class="aboutMe">
       <h1>About Me</h1>
       <p>
         I am a 24 year old male by the name of Justin Ruiz. I'm located in Bergen County, New Jersey and to be more specific, Garfield, NJ. 
         I grew up playing sports such as baseball, swimming, and basketball. All in which helped shaped me to become a leader by going into "battle" 
         with my teammates and having their back through it all. Since then, in whatever I do I always look to be a leader or a resource to my peers 
-        whenever they need a hand. I've never been known as the type to turn my back on anyone. Some of my passions are, but not limited to:
+        whenever they need a hand. I've never been known as the type to turn my back on anyone. It's also important to me to be coach-able and be able to listen
+        and learn from people simply because I know there's always room for improvement! Some of my passions are, but not limited to:
       </p>
       <ul>
           <li>
@@ -26,8 +28,11 @@ export default function Home() {
       </ul>
       <p>
           Growing up a hispanic male has taught me a lot about myself, and others. It's important to me to embrace all the different cultures 
-          presented to me and learn how different these cultures are. 
+          presented to me and learn how different these cultures are. I love getting to know people and learning about who they are and their stories.
+          With that being said, I'd love to get to know you a little more so please be sure to contact me by visiting the contact page above! I will leave you with a 
+          nice image of the person you are reading about, me!
       </p>
+      <img src={Myself} alt="Myself" class ="myself"></img>
     </div>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import PwGenerator from './PasswordGenerator.png';
 
 export default function Projects() {
   return (
     <div>
       <h1>My Projects</h1>
-      <p>
-        INSERT PROJECTS HERE
-      </p>
+      <section class="card col-3 m-3">
+        <section class="card-header">
+            <h2>Password Generator</h2>
+        </section>
+        <section class="card-body bg-secondary">
+            <img src={PwGenerator}></img>
+        </section>
+    </section>
     </div>
   );
 }
