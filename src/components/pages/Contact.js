376c4140c28e6f18413b77aa463d71fe6bb5b596
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-22 12:15:49 -0500

    Added styling but unsure how to cover the entire page with a background image intead of just the div

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,6 +6,8 @@ export default function Contact() {
       <h1>Contact Me</h1>
       <p>
         Email: <a href='mailto: ruiz.justin21@gmail.com'>ruiz.justin21@gmail.com</a>
+      </p>
+      <p>
         Phone Number: <a href='tel: 5517952880'>551-795-2880</a>
       </p>
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,28 @@ export default function Home() {
     <div>
       <h1>About Me</h1>
       <p>
-        INSERT ABOUT ME HERE
+        I am a 24 year old male by the name of Justin Ruiz. I'm located in Bergen County, New Jersey and to be more specific, Garfield, NJ. 
+        I grew up playing sports such as baseball, swimming, and basketball. All in which helped shaped me to become a leader by going into "battle" 
+        with my teammates and having their back through it all. Since then, in whatever I do I always look to be a leader or a resource to my peers 
+        whenever they need a hand. I've never been known as the type to turn my back on anyone. Some of my passions are, but not limited to:
+      </p>
+      <ul>
+          <li>
+              Working out
+          </li>
+          <li>
+              Watching/Playing sports
+          </li>
+          <li>
+              Learning new things
+          </li>
+          <li>
+              Spending time with my loved ones
+          </li>
+      </ul>
+      <p>
+          Growing up a hispanic male has taught me a lot about myself, and others. It's important to me to embrace all the different cultures 
+          presented to me and learn how different these cultures are. 
       </p>
     </div>
   );
